d0cbf73d2fe566642ea5518b0345bca7cb9b93a8
Andrey Rychagov <anryc@yandex.ru>
2018-12-23 19:34:20 +0300

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -46,7 +47,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     FlexLayoutModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
